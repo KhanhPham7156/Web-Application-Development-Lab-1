b9568c1cc4db28a81954c12d7c43e23312895cb5
Khánh Phạm <phamsonggiakhanh2@gmail.com>
2026-02-26 00:56:59 +0700

    feat: add banner, signature coffee section and footer structure

--- Exercise3/assets/js/script.js
+++ Exercise3/assets/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Responsive menu
     const menuToggle = document.getElementById('mobile-menu');
     const nav = document.querySelector('nav');
 
@@ -17,4 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+
+    // chuyển banner bằng nút bấm và auto
+    const leftBtn = document.querySelector('.left-nav-button');
+    const rightBtn = document.querySelector('.right-nav-button');
+    const bannerImages = document.querySelectorAll('.banner-image > div'); // lấy các thẻ div là con của banner-image
+    let currentIndex = 0;
+
+    function showBanner(index) {
+        bannerImages.forEach((img, i) => {
+            if (i === index) {
+                img.classList.add('active');
+            } else {
+                img.classList.remove('active');
+            }
+        });
+    }
+
+    leftBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + bannerImages.length) % bannerImages.length; 
+        showBanner(currentIndex);
+    });
+
+    rightBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % bannerImages.length;
+        showBanner(currentIndex);
+    });
+
+    // Tự động chuyển banner sau 5 giây
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % bannerImages.length; 
+        showBanner(currentIndex);
+    }, 5000);
 });
